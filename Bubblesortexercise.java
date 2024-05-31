613ab7f85a0c2836681bdd333d85dd229bc414e2
Minori <minoriperera1212@gmail.com>
2024-05-31 13:30:17 +0530

    Add files via upload

---
+++ Bubblesortexercise.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datastructuresandalgorithms;
+
+/**
+ *
+ * @author Asus
+ */
+public class Bubblesortexercise {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 2, 1};
+        bubbleSort(arr);
+        printArray(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped)
+                break;
+
+            // Print the array after each outer loop iteration
+            System.out.print("After iteration " + (i + 1) + ": ");
+            printArray(arr);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
